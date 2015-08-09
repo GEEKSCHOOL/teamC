df3f124113ec3cdb0624a88855fb06d8dd0f5266
refactorsheep <kcg.as.2013@gmail.com>
2015-08-09 14:29:55 +0900

    一旦リンク結合

---
+++ html/js/main.js
@@ -0,0 +1,7 @@
+$(".btn").click(function () {
+  $("#image-test").css({"display":"block","opacity":"10"});
+});
+
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
